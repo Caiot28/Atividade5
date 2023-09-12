4ebe9e4ed1557498cd8ce8244a568be61b15c1a5
Caiot28 <141367836+Caiot28@users.noreply.github.com>
2023-09-12 11:08:00 -0300

    Add files via upload

--- Exercicio10.c
+++ Exercicio10.c
@@ -9,35 +9,42 @@ main(){
 
     printf("Digite o valor de a: ");
     scanf("%d", &a);
-
+ 
     printf("Digite o valor de b: ");
     scanf("%d", &b);
 
     printf("Digite o valor de c: ");
     scanf("%d", &c);
 
-    delta = pow(b, 2) - (a * c * 4);
-    
-    printf("Valor de delta = %.0lf", delta);
+    if (a == 0) {
 
-    if (delta == 0) {
+        printf("Nao e equacao de segundo grau");
+
+    } else {
+
+        delta = pow(b, 2) - (a * c * 4);
+    
+        printf("Valor de delta = %.0lf", delta);
+
+        if (delta == 0) {
 
         raiz1 = -b  / (a * 2);
         printf("\nValor da raiz unica = %.2f", raiz1);
 
-    } else if (delta > 0) {
+    }   else if (delta > 0) {
 
         raiz1 = (-b + sqrt(delta)) / (a * 2);
         raiz2 = (-b - sqrt(delta)) / (a * 2);
 
         printf("\nValor das raizes: %.2f e %.2f", raiz1, raiz2);
 
-    } else {
+    }   else {
 
         printf("\nNao e equacao de segundo grau");
 
     }
 
+    }
 
 
 }
