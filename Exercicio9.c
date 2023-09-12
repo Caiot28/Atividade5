9c9fa34f4ce88f852b657cb03a1a35c12ae87bd9
Caiot28 <141367836+Caiot28@users.noreply.github.com>
2023-09-12 10:50:30 -0300

    Add files via upload

--- Exercicio9.c
+++ Exercicio9.c
@@ -3,44 +3,62 @@
 
 main(){
 
-    float salarioAtual, salarioNovo;
-    int tempoServico;
+    float salarioAtual, salarioNovo, bonus, tempoServico;
 
     printf("Digite seu salario: ");
     scanf("%f", &salarioAtual);
 
     printf("Digite seu tempo de servico em anos: ");
-    scanf("%d", &tempoServico);
+    scanf("%f", &tempoServico);
 
-    if (salarioAtual <= 500 && tempoServico < 1) {
+    if (salarioAtual <= 500) {
 
         salarioNovo = salarioAtual + (salarioAtual * 0.25);
-        printf("Seu novo salario e: %.2f", salarioNovo);
 
-    } else if (salarioAtual <= 1000 && tempoServico >= 1 && tempoServico <= 3) {
+    } else if (salarioAtual <= 1000) {
 
-        salarioNovo = salarioAtual + (salarioAtual * 0.2) + 100;
-        printf("Seu novo salario e: %.2f", salarioNovo);
+        salarioNovo = salarioAtual + (salarioAtual * 0.2);
 
-    } else if (salarioAtual <= 1500 && tempoServico >= 4 && tempoServico <= 6) {
+    } else if (salarioAtual <= 1500) {
 
-        salarioNovo = salarioAtual + (salarioAtual * 0.15) + 200;
-        printf("Seu novo salario e: %.2f", salarioNovo);
+        salarioNovo = salarioAtual + (salarioAtual * 0.15);
 
-    } else if (salarioAtual <= 2000 && tempoServico >= 7 && tempoServico <= 10) {
+    } else if (salarioAtual <= 2000) {
 
-        salarioNovo = salarioAtual + (salarioAtual * 0.1) + 300;
-        printf("Seu novo salario e: %.2f", salarioNovo);
-    
-    } else if (salarioAtual > 2000 && tempoServico >= 10) {
-
-        salarioNovo = salarioAtual + 500;
-        printf("Seu novo salario e: %.2f", salarioNovo);
+        salarioNovo = salarioAtual + (salarioAtual * 0.1);
 
     } else {
 
-        printf("Voce nao tem direito a reajuste");
-        
+        salarioNovo = salarioAtual;
+    }
+
+    if (tempoServico >= 1 && tempoServico <= 3) {
+
+        bonus = 100;
+
+    } else if (tempoServico >= 4 && tempoServico <= 6) {
+
+        bonus = 200;
+
+    } else if (tempoServico >= 7 && tempoServico <= 10) {
+
+        bonus =  300;
+
+    } else if (tempoServico > 10) {
+
+        bonus = 500;
+
+    } 
+
+    salarioNovo = salarioNovo + bonus;
+
+    if (salarioNovo == salarioAtual) {
+
+    printf("Nao tem direito a reajuste");
+
+    } else {
+
+        printf("Seu novo salario e: %.2f", salarioNovo);
     }
 
 
